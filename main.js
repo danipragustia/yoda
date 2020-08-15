306d41ac318a2bbb8cda747beb147a82e778ffe0
Dani Pragustia <danipragustia@disroot.org>
2020-08-15 17:53:25 +0700

    update: fix broken when title has backslash or slash.

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ const cli = require('readline').createInterface({
     output: process.stdout
 })
 
-const config = require(process.cwd() + '/config.json')
-
 function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return parseFloat((a/Math.pow(1024,d)).toFixed(c))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
 
 const menu = x => x.map((x,y) => console.log(y + 1 + '. ' + x))
@@ -44,22 +42,26 @@ const download = (link, itag, container) => new Promise((resolve, reject) => {
     })
 })
 
-const fetchBest = (link, videos, audios) => new Promise((resolve, reject) => {
+const fetchBest = (link, videos, audios, title) => new Promise((resolve, reject) => {
 
     if (videos.length > 1) {
-	video = videos[0]
-	audio = audios[0]
-	console.log('[+] The best video quality selected : ' + video.qualityLabel + ' (' + video.container + ' ' + video.videoCodec + ')')
-	console.log('[+] The best audio quality selected : ' + audio.container + ' ' + audio.audioCodec + ' (' + audio.audioBitrate + 'kbps)')
+
+	let video = videos.filter(x => x.container == 'mp4')[0]
+	let audio = audios[0]
 
 	download(link, video.itag, video.container).then((res) => {
 	    download(link, audio.itag, audio.container).then((res) => {
 		console.log('[+] Rendering the video, it tooks time...')
 		let videopath = path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '-' + video.itag + '.' + video.container)
 		let audiopath = path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '-' + audio.itag + '.' + audio.container)
+		let outputpath = path.join(process.cwd(), '/' + title.replace('/','_').replace('\\','_') + '.mp4') 
+		
+		console.log('[+] Video quality selected : ' + video.qualityLabel + ' (' + video.container + ' ' + video.videoCodec + ')')
+		console.log('[+] Audio quality selected : ' + audio.container + ' ' + audio.audioCodec + ' (' + audio.audioBitrate + 'kbps)')
+		console.log('[+] Transcoding : ' + (video.container == 'mp4' ? 'not needed' : 'libx264'))
 		let command = ffmpeg(videopath)
 		    .input(audiopath)
-		    .videoCodec('copy')
+		    .videoCodec((video.container == 'mp4' ? 'copy' : 'libx264'))
 		    .audioCodec('libopus')
 		    .on('progress', progress => {
 			console.log('[+] Progress : ' + progress.percent.toFixed() + '%')
@@ -74,9 +76,9 @@ const fetchBest = (link, videos, audios) => new Promise((resolve, reject) => {
 			fs.unlinkSync(videopath)
 			fs.unlinkSync(audiopath)
 			console.log('[+] Processing finished !')
-			resolve(path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '.mp4'))
+			resolve(outputpath)
 		    })
-		    .save(path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '.mp4'))
+		    .save(outputpath)
 	    })
 	})
 	
@@ -84,13 +86,14 @@ const fetchBest = (link, videos, audios) => new Promise((resolve, reject) => {
 
 })
 
-const fetch = (link, videos, audios) => new Promise((resolve, reject) => {
+const fetch = (link, videos, audios, title) => new Promise((resolve, reject) => {
 
     if (videos.length > 1) {
 	//console.log(videos)
 	//console.log(audios)
-	videos.map((x,y) => console.log((y + 1) + '. ' + x.qualityLabel + ' (' + x.container + ' ' + x.videoCodec + ') ['  + formatBytes(x.contentLength) + ']'))
-	cli.question('\nSelect the video quality (1-' + videos.length + ') : ', async video => {
+	console.log('[+] Select the quality of video on below')
+	videos.map((x,y) => console.log('  ' + (y + 1) + '. ' + x.qualityLabel + ' (' + x.container + ' ' + x.videoCodec + ') ['  + formatBytes(x.contentLength) + ']'))
+	cli.question('\n(1-' + videos.length + ') : ', async video => {
 
 	    if (videos[video - 1] === 'undefined') {
 
@@ -98,8 +101,10 @@ const fetch = (link, videos, audios) => new Promise((resolve, reject) => {
 		main()
 		
 	    } else {
-		audios.map((x,y) => console.log((y + 1) + '. ' + x.container + ' ' + x.audioCodec + ' (' + x.audioBitrate + 'kbps) [' + formatBytes(x.contentLength) + ']'))
-		cli.question('\nSelect the audio quality (1-' + audios.length + ') : ', async audio => {
+		console.log()
+		console.log('[+] Select the quality of audio on below')
+		audios.map((x,y) => console.log('  ' + (y + 1) + '. ' + x.container + ' ' + x.audioCodec + ' (' + x.audioBitrate + 'kbps) [' + formatBytes(x.contentLength) + ']'))
+		cli.question('\n(1-' + audios.length + ') : ', async audio => {
 
 		    if (audios[audio - 1] === 'undefined') {
 
@@ -113,9 +118,13 @@ const fetch = (link, videos, audios) => new Promise((resolve, reject) => {
 				console.log('[+] Rendering the video, it tooks time...')
 				let videopath = path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '-' + videos[video - 1].itag + '.' + videos[video - 1].container)
 				let audiopath = path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '-' + audios[audio - 1].itag + '.' + audios[audio - 1].container)
+				let outputpath = path.join(process.cwd(), '/' + title.replace('/','_').replace('\\','_') + '.mp4')
+				console.log('[+] Video quality selected : ' + videos[video - 1].qualityLabel + ' (' + videos[video - 1].container + ' ' + videos[video - 1].videoCodec + ')')
+				console.log('[+] Audio quality selected : ' + audios[audio - 1].container + ' ' + audios[audio - 1].audioCodec + ' (' + audios[audio - 1].audioBitrate + 'kbps)')
+				console.log('[+] Transcoding : ' + (videos[video - 1].container == 'mp4' ? 'not needed' : 'libx264'))
 				let command = ffmpeg(videopath)
 				    .input(audiopath)
-				    .videoCodec('copy')
+				    .videoCodec((videos[video - 1].container == 'mp4' ? 'copy' : 'libx264'))
 				    .audioCodec('libopus')
 				    .on('progress', progress => {
 					console.log('[+] Progress : ' + progress.percent.toFixed() + '%')
@@ -130,9 +139,9 @@ const fetch = (link, videos, audios) => new Promise((resolve, reject) => {
 					fs.unlinkSync(videopath)
 					fs.unlinkSync(audiopath)
 					console.log('[+] Processing finished !')
-					resolve(path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '.mp4'))
+					resolve(outputpath)
 				    })
-				    .save(path.join(process.cwd(), '/' + ytdl.getURLVideoID(link) + '.mp4'))
+				    .save(outputpath)
 			    })
 			})
 			
@@ -162,9 +171,9 @@ async function main() {
     cli.question('> ', async select => {
 	if (select == 1) {
 	    cli.question('Link Video : ', async link => {
-		console.log('[+] Fetching video(s), please wait warmly...')
+		console.log('[+] Fetching video(s), please wait warmly...\n')
 		let info = await ytdl.getInfo(ytdl.getURLVideoID(link))
-		fetch(link, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly')).then((res) => {
+		fetch(link, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly'), info.videoDetails.title).then((res) => {
 		    console.log('[+] File location : ' . res)
 		    console.log('[+] Done')
 		    main()
@@ -173,7 +182,7 @@ async function main() {
 	} else if (select == 2) {
 	    cli.question('ID Playlist / Channel (bukan URL) : ', async id => {
 		await ytpl(id).then(async res => {
-		    console.log('[+] There ' + res.total_items + ' video(s) ready to download')
+		    console.log('[+] There ' + res.total_items + ' video(s) ready to download\n')
 		    menu([
 			'Automatically highest one',
 			'Manually for every single time'
@@ -183,9 +192,9 @@ async function main() {
 			if (options == 1) {
 			    await res.items.reduce(
 				(p, video) => p.then(async () => {
-				    console.log('[+] Fetching video(s), please wait warmly...')
+				    console.log('[+] Fetching video(s), please wait warmly...\n')
 				    let info = await ytdl.getInfo(ytdl.getURLVideoID(video.url_simple))
-				    await fetchBest(video.url_simple, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly')).then((res) => {
+				    await fetchBest(video.url_simple, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly'),info.videoDetails.title).then((res) => {
 					console.log('[+] File location : ' . res)
 				    })
 				}),startedPromise
@@ -195,9 +204,9 @@ async function main() {
 			} else if (options == 2) {
 			    await res.items.reduce(
 				(p, video) => p.then(async () => {
-				    console.log('[+] Fetching video(s), please wait warmly...')
+				    console.log('[+] Fetching video(s), please wait warmly...\n')
 				    let info = await ytdl.getInfo(ytdl.getURLVideoID(video.url_simple))
-				    await fetch(video.url_simple, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly')).then((res) => {
+				    await fetch(video.url_simple, ytdl.filterFormats(info.formats, 'videoonly'), ytdl.filterFormats(info.formats, 'audioonly'),info.videoDetails.title).then((res) => {
 					console.log('[+] File location : ' . res)
 				    })
 				}),startedPromise
